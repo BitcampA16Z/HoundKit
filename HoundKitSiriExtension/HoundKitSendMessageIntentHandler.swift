f1b9d35913c9c8444d56c408faba8d6d728563c7
Colin King <colin.king.96@gmail.com>
2016-06-18 20:07:31 -0700

    Renamed HoundKit from AlexaKit

---
+++ HoundKitSiriExtension/HoundKitSendMessageIntentHandler.swift
@@ -0,0 +1,56 @@
+//
+//  HoundKitSendMessageIntentHandler.swift
+//  HoundKit
+//
+//  Created by Colin King on 6/18/16.
+//  Copyright © 2016 Colin King. All rights reserved.
+//
+
+import Foundation
+import Intents
+
+class HoundKitSendMessageIntentHandler: INExtension, INSendMessageIntentHandling {
+    
+    let NAME = "Hound"
+    let APP_NAME = "HoundKit"
+    
+    func resolveRecipients(forSendMessage intent: INSendMessageIntent, with completion: ([INPersonResolutionResult]) -> Swift.Void) {
+        print("forcing resolve to recipient: " + NAME)
+        
+        let hound = INPerson.init(handle: NAME, displayName: NAME, contactIdentifier: NAME)
+        let resolutionResults = [INPersonResolutionResult.success(with: hound)]
+        completion(resolutionResults)
+    }
+    
+    func resolveContent(forSendMessage intent: INSendMessageIntent, with completion: (INStringResolutionResult) -> Swift.Void) {
+        print("resolving content")
+        if let text = intent.content where !text.isEmpty {
+            print("received text...")
+            print(text)
+            completion(INStringResolutionResult.success(with: text))
+        }
+        else {
+            print("needs content")
+            completion(INStringResolutionResult.needsValue())
+        }
+    }
+    
+    func handle(sendMessage intent: INSendMessageIntent, completion: (INSendMessageIntentResponse) -> Void) {
+        print("Message intent is being handled.")
+        if let query = intent.content {
+            sendMessage(query: query)
+        } else {
+            print("ERROR: Empty message")
+        }
+        let userActivity: NSUserActivity? = nil // NSUserActivity(activityType: NSStringFromClass(INSendMessageIntent))
+        let response = INSendMessageIntentResponse(code: .success, userActivity: userActivity)
+        completion(response)
+    }
+    
+    func sendMessage(query: NSString) {
+        print("Sending message")
+        print(query)
+        // Send this message to the API server, retrieve response
+        
+    }
+}
